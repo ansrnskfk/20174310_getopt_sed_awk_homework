b7c6fa194d8faa2f84fed6503d7d71bba8ca3ae3
geun <leegg98@gmail.com>
2021-11-13 17:29:52 +0900

    add -p, -a, -f and -h option at getopt.sh

--- getopt.sh
+++ getopt.sh
@@ -2,9 +2,25 @@
 
 help()
 {
+echo "<options>"
 	echo "	-h	도움말"
 	echo "	-p ARG	받은 인자를 출력해주는  opt"
 	echo "	-f	fruit.txt를 보여주는 opt"
 	echo "	-a	animal.txt.를 보여주는 opt"
 }
 
+while getopts "p:fah" opt
+do
+	case $opt in
+	p) echo "argument: ${OPTARG}"
+	;;
+	f) cat fruit.txt
+	;;
+	a) cat animal.txt
+	;;
+	h) help
+	;;
+	\?) echo "invalid option. try to -h option"
+	;;
+	esac
+done
