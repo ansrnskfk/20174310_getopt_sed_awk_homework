9b6a938682833d1d99efd4d17bf6eeadc2930ef8
geun <leegg98@gmail.com>
2021-11-13 19:33:04 +0900

    add test awk.sh

--- awk.sh
+++ awk.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+echo "cat student.txt"
+cat student.txt
 
+echo -e "\n1. awk '{print $1,$3}' student.txt"
+awk '{print $1,$3}' student.txt
 
+echo -e "\n2. awk '/P/' student.txt"
+awk '/P/' student.txt
+
+echo -e "\n3. awk '{if ($2 > 2 && $3 == "P") print ($0)}' student.txt"
+awk '{if ($2 > 2 && $3 == "P") print ($0)}' student.txt
